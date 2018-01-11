c5b70deda16cd6a8e2afaf2e01a4a8c5ee56274a
Luis Fernando <foluis@hotmail.com>
2018-01-10 19:51:34 -0600

    Add project files.

---
+++ TokenAutenticationExample/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TokenAutenticationExample.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Auth")]
+    public class AuthController :  ControllerBase
+    {
+        //Get Anonymous Resource
+        [HttpPost("[action]")]
+        [AllowAnonymous]
+        public string GetValues()
+        {
+            return "Resource";
+        }
+
+        //Get Authorized Resource
+        //Log in (authenticate)
+        //Get Authorized Resource Rol Admin
+        //Get Authorized Resource Rol Supervisor
+    }
+}
